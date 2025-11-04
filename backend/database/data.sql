55f64e7a885b72ffa015dca864277933927ccfac
Liam <liamoliva1@gmail.com>
2025-11-04 18:55:25 -0500

    Real charities + images added to database

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -1,42 +1,122 @@
 INSERT INTO charities (name, tags, description, website, location, filename)
 VALUES
   (
-    'Charity 1',
-    '["Education", "Youth"]',
-    'Supports educational programs for underprivileged children.',
-    'https://example.org/charity1',
-    'New York, USA',
-    'placeholder.jpg'
-  ),
-  (
-    'Charity 2',
+    'Planned Parenthood of Michigan',
     '["Health", "Women"]',
-    'Provides healthcare access for women in rural areas.',
-    'https://example.org/charity2',
-    'Lagos, Nigeria',
-    'placeholder.jpg'
+    'Planned Parenthood of Michigan is a non-profit in Ann Arbor. The organization offers crucial reproductive health services, education, and advocacy, guaranteeing easy access to care and enabling people to make knowledgeable health choices.',
+    'https://www.plannedparenthood.org/planned-parenthood-michigan',
+    'Ann Arbor, USA',
+    'img1.jpg'
   ),
   (
-    'Charity 3',
-    '["Environment", "Conservation"]',
-    'Protects forests and promotes sustainable farming practices.',
-    'https://example.org/charity3',
-    'São Paulo, Brazil',
-    'placeholder.jpg'
+    'Humane Society of Huron Valley',
+    '["Animal"]',
+    'Humane society in Michigan that offers shelter, medical care, and adoption services for animals, along with community education on humane treatment and responsible pet ownership. It is one of the largest animal shelters in Michigan.',
+    'https://www.hshv.org/',
+    'Ann Arbor, USA',
+    'img2.png'
   ),
   (
-    'Charity 4',
-    '["Animal Welfare", "Community"]',
-    'Rescues abandoned animals and supports local shelters.',
-    'https://example.org/charity4',
-    'Toronto, Canada',
-    'placeholder.jpg'
+    'Ann Arbor Art Center',
+    '["Community", "Arts"]',
+    'A non-profit organization in Ann Arbor. Promotes artistic creativity through exhibitions, workshops, and educational programs, supporting local artists and fostering a vibrant arts community in Ann Arbor.',
+    'https://www.annarborartcenter.org/',
+    'Ann Arbor, USA',
+    'img3.png'
   ),
   (
-    'Charity 5',
-    '["Global Aid", "Disaster Relief"]',
-    'Delivers emergency aid and supplies during natural disasters.',
-    'https://example.org/charity5',
-    'Tokyo, Japan',
-    'placeholder.jpg'
+    'The Creature Conservancy',
+    '["Animal", "Education", "Environment"]',
+    'The Creature Conservancy is a non-profit wildlife education organization in Ann Arbor, Michigan.',
+    'https://thecreatureconservancy.org/',
+    'Ann Arbor, USA',
+    'img4.jpeg'
+  ),
+  (
+    'Home of New Vision',
+    '["Health", "Community"]',
+    'Home of New Vision, a charity in Ann Arbor, offers comprehensive addiction treatment and recovery services to support long-term sobriety.',
+    'https://homeofnewvision.org/',
+    'Ann Arbor, USA',
+    'img5.jpg'
+  ),
+  (
+    'Michigan Ovarian Cancer Alliance',
+    '["Health", "Cancer"]',
+    'MIOCA promotes the early detection of ovarian cancer and improved treatment outcomes. MIOCA raises awareness of ovarian cancer, provides resources and support to survivors and their families. It is a cancer care nonprofit in Ann Arbor.',
+    'https://mioca.org/',
+    'Ann Arbor, USA',
+    'img6.png'
+  ),
+  (
+    'National Network of Depression Centers',
+    '["Mental Health", "Community"]',
+    'The National Network of Depression Centers is a charity based in Ann Arbor, United States, that promotes research, education, and treatment for depression and related mood disorders.',
+    'https://nndc.org/',
+    'Ann Arbor, USA',
+    'img7.jpg'
+  ),
+  (
+    'Hands Across the Water',
+    '["Children"]',
+    'Hands Across the Water, Inc. focuses on improving the lives of children in need, offering support through education, healthcare, and community programs both locally and internationally.',
+    'https://www.hatw.org/',
+    'Ann Arbor, USA',
+    'img8.jpg'
+  ),
+  (
+    'Shelter Association of Washtenaw County',
+    '["Homelessness"]',
+    'The Shelter Association of Washtenaw County, situated in the heart of Ann Arbor, is dedicated to offering support and housing solutions to those facing homelessness. Our diverse range of programs, including Residential, Non-Residential/Shelter Diversion, Critical-Time Intervention, Pathways Program, The Bail Project, Recuperative Care, Rotating Shelter, Housing Crisis Stabilization, and Winter Programs, cater to various needs within our community. Committed to proven strategies, community education, advocacy, and comprehensive services, we strive to provide homes and hope to individuals and families across Southeast Michigan.',
+    'https://www.annarborshelter.org/',
+    'Ann Arbor, USA',
+    'img9.png'
+  ),
+  (
+    'Peace Neighboorhood Center',
+    '["Community"]',
+    'Peace Neighborhood Center’s mission is to provide programs for children, families, and individuals who are affected by social and economic problems. Peace helps people discover options, enhance skills, and make choices that lead to self-sufficiency and positive community involvement.',
+    'https://peaceneighborhoodcenter.org/',
+    'Ann Arbor, USA',
+    'img10.png'
+  ),
+  (
+    'The Breakfast Program at St. Andrew''s',
+    '["Poverty", "Hunger"]',
+    'A community providing a daily hot breakfast and welcoming space to anyone who is in need. We do this to nourish and strengthen all those who come through our doors.',
+    'https://breakfastatstandrews.org/',
+    'Ann Arbor, USA',
+    'img11.ong'
+  ),
+  (
+    'The Business of Doing Good',
+    '["Education", "Community"]',
+    'We inspire kids to discover what is truly meaningful to them—and empower them with the skills to put their ideas into action through social entrepreneurship.',
+    'https://www.thebusinessofdoinggood.org/',
+    'Ann Arbor, USA',
+    'img12.jpeg'
+  ),
+  (
+    'Fostering Futures',
+    '["Children"]',
+    'Fostering Futures is a non-profit in Ann Arbor that supports youth in foster care by providing resources, advocacy, and educational opportunities to help them transition to independent adulthood successfully.',
+    'https://www.ffkids.org/',
+    'Ann Arbor, USA',
+    'img13.jpg'
+  ),
+  (
+    'Amara Conservation',
+    '["Environment", "Education"]',
+    'Amara Conservation promotes conservation awareness, protecting wildlife and natural resources with Michigan-based and global initiatives. It is an environmental protection nonprofit in Michigan.',
+    'https://www.amaraconservation.org/',
+    'Ann Arbor, USA',
+    'img14.png'
+  ),
+  (
+    'Concert Music Outreach Collective',
+    '["Community", "Music"]',
+    'CMOC was created to fight the injustices in access to classical music by performing concerts and sharing our love of music with all members of society.',
+    'https://cmoutreachcollective.org/',
+    'Ann Arbor, USA',
+    'img15.jpg'
   );
